2d5e48cf65d8f4d8fd3b09a169723b2d8e03c920
Francisco Ramirez <francisco@arex.ai>
2024-10-27 23:57:36 -0600

    refactor(helpers): :recycle: transfer transformation features to helper function

--- helpers.py
+++ helpers.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
-def get_dataframe(conn, query):
-    return pd.read_sql(query, conn).to_dict(orient='records')
-    
+def get_journalRecords(conn, query):
+    # Extract data
+    df: pd.DataFrame = pd.DataFrame(pd.read_sql(query, conn).to_dict(orient='records'))
+
+    api_columns = {
+        'oracle_bank_transactions_general_ledger_transaction_id': 'journal_transaction_id',
+        'oracle_bank_transactions_general_ledger_amt': 'transaction_amount',
+        'match_status': 'audit_control',
+    }
+
+    # Transform
+    journal_records = df[api_columns.keys()].copy(deep=True)
+    journal_records.rename(columns=api_columns, errors="raise", inplace=True)
+    journal_records['audit_control'] = journal_records['audit_control'].apply(lambda x: True if x == 'MATCH' else False)
+
+    return journal_records.to_dict(orient='records')
--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 # main.py
 import requests 
 from config import conn
-from helpers import get_dataframe
+from helpers import get_journalRecords
 
 
 # Extract data from source
-query = "SELECT * FROM sales.public.df_comp_test101 LIMIT 10"
-df = get_dataframe(conn=conn, query=query)
+query = "SELECT * FROM sales.public.df_comp_test101 LIMIT 30"
+payload = get_journalRecords(conn=conn, query=query)
 
-# Transform data
-payload = []
-
-for record in df:
-    payload_record = {}
-
-    payload_record['journal_transaction_id'] = record['oracle_bank_transactions_general_ledger_transaction_id']
-    payload_record['transaction_amount'] = record['oracle_bank_transactions_general_ledger_amt']
-    payload_record['audit_control'] = 1 if record['match_status'] == 'MATCH' else 0
-
-    payload.append(payload_record)
+print(payload)
 
 # Load data to API endpoint
 url = 'http://localhost:8000/journal-records/'
@@ -26,5 +16,5 @@ url = 'http://localhost:8000/journal-records/'
 response = requests.post(url, json=payload)
 
 ## print response status and content
-print(f"Status Code: {response.status_code}")
+print(f"Status Code: {response.status_code}\n\n")
 print(f"Response content: {response.json()}")
